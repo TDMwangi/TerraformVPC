bc04538edd29c439bdbb07578284501ef13bfe7b
TDMwangi <teddymwangi65@gmail.com>
2023-12-08 10:23:30 +0300

    feat: add variable public_subnets

--- variables.tf
+++ variables.tf
@@ -20,3 +20,11 @@ variable "private_subnets" {
     "private_subnet_3" = 3
   }
 }
+
+variable "public_subnets" {
+  default = {
+    "public_subnet_1" = 1
+    "public_subnet_2" = 2
+    "public_subnet_3" = 3
+  }
+}
