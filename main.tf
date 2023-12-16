005f57881d02108d0025dc4d4e2dead6832f158c
TDMwangi <teddymwangi65@gmail.com>
2023-12-16 09:16:05 +0300

    feat: add vpc block

--- main.tf
+++ main.tf
@@ -5,3 +5,14 @@ provider "aws" {
 # Retrieve the list of availability zones in the region
 data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
+
+# Define the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name        = var.vpc_name
+    Environment = "Development"
+    Terraform   = true
+  }
+}
