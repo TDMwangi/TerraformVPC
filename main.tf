6ce1666565b69160ea19babf75a4eee908afcc3c
TDMwangi <teddymwangi65@gmail.com>
2023-12-24 08:19:22 +0300

    feat: add internet_gateway block

--- main.tf
+++ main.tf
@@ -89,3 +89,13 @@ resource "aws_route_table_association" "private" {
   for_each       = aws_subnet.private_subnets
   subnet_id      = each.value.id
 }
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name      = "teddy_igw"
+    Terraform = true
+  }
+}
